903cf13ba344cea816bf7d68ee95f26124597350
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2024-02-21 00:33:55 +0530

    Update start.py

--- nksama/plugins/start.py
+++ nksama/plugins/start.py
@@ -41,9 +41,11 @@ def start(_, message):
                 [[InlineKeyboardButton('help', callback_data="help")]]))
     if "help" in message.text:
         bot.send_message(message.chat.id,
-                         "Help",
+                         "Hey, you can check my commands here! go throught the inline's below!",
                          reply_markup=InlineKeyboardMarkup([[
                              InlineKeyboardButton('help', callback_data="help")
                          ]]))
     if not message.chat.type == "private":
-        message.reply("Here is my command help section!")
+        message.reply("Hello {message.from_user.mention} I'm Kotoko Iwanaga[.](https://telegra.ph/file/c34f0d3f93608eb5faa17.jpg)\nI'll help you to manage your groups at ease\nAdd Me in your Group with full rights i will be a good assistant!!",
+                reply_markup=InlineKeyboardMarkup(
+                    [[InlineKeyboardButton('Help', callback_data="help")]]))
