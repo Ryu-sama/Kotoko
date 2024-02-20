659b915113fa7b1a2437fb68aeafb434e5b55ce7
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2024-02-20 23:49:47 +0530

    Update __init__.py

--- nksama/__init__.py
+++ nksama/__init__.py
@@ -17,8 +17,8 @@ help_message = []
 #         help.update({f"{module}_help": help})
 
 bot = Client('bot',
-             api_id=os.environ.get('API_ID'),
-             api_hash=os.environ['API_HASH'],
-             bot_token=os.environ['BOT_TOKEN'],
+             api_id="14676558",
+             api_hash="b3c4bc0ba6a4fc123f4d748a8cc39981",
+             bot_token="6911807700:AAH6ibUG_0YVJmULoZlioMIZyzRcUFTXWK4",
              plugins=dict(root=f"{__name__}/plugins"))
 
