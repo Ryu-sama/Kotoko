32149127cb482a913ca25ba821fd4d88739cd689
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2024-02-20 23:47:58 +0530

    Update db.py

--- nksama/db.py
+++ nksama/db.py
@@ -1,6 +1,6 @@
 import pymongo
 import os
 
-MONGO_URL = os.environ.get('MONGO_URL')
+MONGO_URL = "mongodb+srv://enmu:enmu123@enmu.2cuev.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 
 database = pymongo.MongoClient(MONGO_URL)['notes']['notes']
