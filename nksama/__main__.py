b0d69f04947b91cdced2643e10841f980d9cc58d
Ryu_sama <90622022+Ryu120@users.noreply.github.com>
2024-02-21 00:38:55 +0530

    Update __main__.py

--- nksama/__main__.py
+++ nksama/__main__.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     bot.run()
     with bot:
-        bot.send_message(-1001544622735 , "I'm Now online")
+        bot.send_message(-1001594147818 , "wisdom")
     
 
 
